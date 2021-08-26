34ae00adca9dce2e2bb74287e72c09999abbcfdc
jxstxn__ <Justinbaumann12112001@gmail.com>
2021-08-26 14:19:00 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_awesome_notifications_tutorial/home_page.dart';
+
+void main() {
+  AwesomeNotifications().initialize(
+    'resource://drawable/res_notification_app_icon',
+    [
+      NotificationChannel(
+        channelKey: 'basic_channel',
+        channelName: 'Basic Notifications',
+        defaultColor: Colors.teal,
+        importance: NotificationImportance.High,
+        channelShowBadge: true,
+      ),
+      NotificationChannel(
+        channelKey: 'scheduled_channel',
+        channelName: 'Scheduled Notifications',
+        defaultColor: Colors.teal,
+        locked: true,
+        importance: NotificationImportance.High,
+        soundSource: 'resource://raw/res/_custom_notification',
+      ),
+    ],
+  );
+  runApp(AppWidget());
+}
+
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.teal,
+        accentColor: Colors.tealAccent,
+      ),
+      title: 'Green Thumbs',
+      home: HomePage(),
+    );
+  }
+}
